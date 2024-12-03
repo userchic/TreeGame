acd08fccbf8d055ceae44dac5d1338c28fe42faf
Ruslan <r.gabzalil@mail.ru>
2024-12-03 22:31:39 +0500

    Добавлена генерация деревьев

---
+++ WpfApp1/Elements/Tree.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Tree.xaml
+    /// </summary>
+    public partial class Tree : UserControl
+    {
+        public delegate void Handler();
+        public event Handler Click;
+        public Tree()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke();
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Elements;
 
 namespace WpfApp1
 {
@@ -20,16 +21,75 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Random rand=new Random();
         public MainWindow()
         {
             InitializeComponent();
         }
-        public void CreateField(object sender,EventArgs e)
+        bool [,] Trees;
+        int[] horizontalNumbers;
+        int[] verticalNumbers;
+        public void CreateRandomField(object sender,EventArgs e)
         {
-            //устанавливаем размер грида
+            //обнуляем и устанавливаем размер грида
+            ClearGrid();
+            SetGridSize(11, 11);
+            //в гриде устанавливаем деревья с помощью генератора случайнызх чисел
+            Trees = new bool[10, 10];
+            RandomizeField(30);
+            PlaceFieldTrees();
+            InitializeNumbers();
+        }
 
-            //в гриде устанавливаем элементы с помощью генератора случайнызх чисел
-            grid.Background = new SolidColorBrush(Color.FromRgb(100, 100, 100));
+        private void InitializeNumbers()
+        {
+            for(int i=0;i<Trees.GetLength(0);i++)
+            {
+                
+            }
+        }
+
+        private void PlaceFieldTrees()
+        {
+            for (int i = 0; i < Trees.GetLength(0); i++)
+            {
+                for (int j = 0; j < Trees.GetLength(1); j++)
+                {
+                    if (Trees[i, j] == true)
+                    {
+                        Tree tree = new Tree();
+                        Grid.SetColumn(tree, i);
+                        Grid.SetRow(tree, j);
+                        grid.Children.Add(tree);
+                    }
+                }
+            } 
+        }
+
+        public void ClearGrid()
+        {
+            grid.Children.Clear();
+            grid.ColumnDefinitions.Clear();
+            grid.RowDefinitions.Clear();
+        }
+        public void SetGridSize(int horizontalN,int verticalN )
+        {
+            for (int i = 0; i < horizontalN; i++)
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            for (int i = 0; i < verticalN; i++)
+                grid.RowDefinitions.Add(new RowDefinition());
+        }
+        private void RandomizeField(int trees)
+        {
+            for (int i=0;i<trees; i++)
+            {
+                int x, y;
+                do
+                {
+                    (x, y) = (rand.Next(10), rand.Next(10));
+                } while (Trees[x, y] == true);
+                Trees[x, y] = true;
+            }
         }
     }
 }
